23bfe2f04ffd3771e9b1396dd67fa5aaa097da54
ankpandit <79821628+ankpandit@users.noreply.github.com>
2021-03-07 17:10:24 +0530

    Add files via upload

---
+++ TUT1.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#qus1 WAP to check if 10 is greater than 2 or not 
+if 10>2:
+    print("yes it is true")
+else:
+    print("not true ")
+
+
+# In[2]:
+
+
+#qus2 print variables x,y,z having datatypes int , string , boolean
+a=5
+b="hello"
+c=True
+print(a,b,c)
+
+
+# In[ ]:
+
+
+#qus3 print 3 variables x,y,z using assignment with multiples
+a,b,c=5,"hello",5.0
+
+
+# In[ ]:
+
+
+#qus4 comment the qus 1 and qus2 with their respective qus 
+
+
+# In[3]:
+
+
+#qus5 print the datatype of variable defined in qus 2
+a=5
+b="hello"
+c=True
+print(type(a),type(b),type(c))
+
+
+# In[4]:
+
+
+#qus6 print the variable fullfiling the criteria abcd of variable name
+_abcd1="ehllo0"
+print(_abcd1)
+
+
+# In[5]:
+
+
+#qus7 WAP to print multiple variable then its datatype 
+a,b,c,d=5,5.0,"hello",True
+print(a,b,c,d)
+print(type(a),type(b),type(c),type(d))
+
+
+# In[7]:
+
+
+"""qus8 print the datatype of
+a="hello"
+b=20
+c=20.5
+d=["a","b","C"]
+e=range(6)
+f={1:"apple",2:"orange"}
+g=True
+h=list(("a","b","c"))
+i=tuple(["a","b","c"])
+j=dict(j=key="1",value="apple")"""
+a="hello"
+b=20
+c=20.5
+d=["a","b","C"]
+e=range(6)
+f={1:"apple",2:"orange"}
+g=True
+h=list(("a","b","c"))
+i=tuple(["a","b","c"])
+j=dict(key="1",value="apple")
+print(type(a),type(b),type(c))
+
+
+# In[10]:
+
+
+#print 3rd and 4th char from the string from the ending and starting  
+a="hello world"
+print(a[-3],a[3],sep="   ")
+
+
+# In[ ]:
+
+
+
+
